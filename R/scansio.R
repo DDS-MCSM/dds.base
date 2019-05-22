913cf597d0861d8b28e198ae5afd12b4d7fb4bfc
Humbert <humbert.costas@gmail.com>
2019-05-23 00:21:11 +0200

    Added getScansioFTPs function, this will do all process and return tidy data.frame of FTP connections between two IPv4 addresses and its geolocation information.

--- R/scansio.R
+++ R/scansio.R
@@ -28,3 +28,73 @@ download.ftp.scans.io <- function(dirdata = "data") {
 
   return(df.tcp21)
 }
+
+#' Load FTP data set from Rapid7 and add IP geolocation data, providing a tidy data set.
+#'
+#' @param scope number of observations. Warning: if scope > 1000 it will use multiple cores.
+#' @param dirdata path where raw data and tidy data frames will be stored
+#' @param seed used to select the scope observations
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ftps <- getScansioFTPs()
+#' }
+getScansioFTPs <- function(scope = 500, dirdata = "data", seed = 666) {
+  set.seed(seed)
+  dir.data <- file.path(getwd(), dirdata)
+  if (!dir.exists(dir.data)) {
+    dir.create(dir.data)
+  }
+
+  print("[*] Load source data sets")
+  if (file.exists(file.path(dirdata, "scansio.tcp21.rds"))) {
+    df <- readRDS(file.path(dirdata, "scansio.tcp21.rds"))
+  } else {
+    df <- download.ftp.scans.io(dirdata)
+  }
+  if (file.exists(file.path(dirdata, "maxmind.rds"))) {
+    df.maxmind <- readRDS(file.path(dirdata, "maxmind.rds"))
+  } else {
+    df.maxmind <- download.maxmind(dirdata)
+  }
+
+  print("[*] Prepare scans.io data.frame")
+  # Seleccionamos una muestra de scans
+  df <- df[sample(1:nrow(df), scope),]
+
+  # Transformamos las IPs a formato decimal
+  df$saddr.num <- iptools::ip_to_numeric(df$saddr)
+  df$daddr.num <- iptools::ip_to_numeric(df$daddr)
+
+  print("[*] Find IP geolocation data")
+  # Geolocalizamos las IPs origen y destino
+  geo.src <- addIPgeolocation(ips = df$saddr,
+                              df.maxmind = df.maxmind,
+                              boost = scope > 1000)
+  geo.dst <- addIPgeolocation(ips = df$daddr,
+                              df.maxmind = df.maxmind,
+                              boost = scope > 1000)
+
+  print("[*] Tidy data frame")
+  names(geo.src) <- paste("src_", names(geo.src), sep = "")
+  names(geo.dst) <- paste("dst_", names(geo.dst), sep = "")
+  # Preparamos el data frame
+  df <- dplyr::bind_cols(df, geo.src, geo.dst)
+  df$dst_is_anonymous_proxy <- as.factor(df$dst_is_anonymous_proxy)
+  df$src_is_anonymous_proxy <- as.factor(df$src_is_anonymous_proxy)
+  df$dst_is_satellite_provider <- as.factor(df$dst_is_satellite_provider)
+  df$src_is_satellite_provider <- as.factor(df$src_is_satellite_provider)
+
+  df <- dplyr::select(df, timestamp_ts, ttl,
+                      saddr, sport, src_network, src_latitude, src_longitude,
+                      src_accuracy_radius, src_is_anonymous_proxy, src_is_satellite_provider,
+                      daddr, dport, dst_network, dst_latitude, dst_longitude,
+                      dst_accuracy_radius, dst_is_anonymous_proxy, dst_is_satellite_provider)
+
+  saveRDS(object = df, file = file.path(dir.data, "df_ftps.rds"))
+
+  return(df)
+}
