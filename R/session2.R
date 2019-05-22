25716b3484241db6064ff99b2c564b3ea8322e72
Humbert <humbert.costas@gmail.com>
2019-05-23 00:06:08 +0200

    New function addIPgeolocation, search IPs in MaxMind ranges to get its geolocation information. Working but too slow, needs code review to improve performance.

--- R/session2.R
+++ R/session2.R
@@ -36,3 +36,53 @@ download.maxmind <- function(dirdata = "data") {
 
   return(df.maxmind)
 }
+
+#' Given a vector of IP addresses it returns a data frame with
+#' the IP addresses and its geolocation data (lat, long, accuracy and more).
+#'
+#' @param ips array of characters of IPv4 addresses
+#' @param df.maxmind data frame from download.maxmind function
+#' @param boost logical default set as FALSE, if TRUE it will use parallel computing using multiple cores
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' geoips <- addIPgeolocation(ips = c("8.8.8.8", "147.81.23.1"),
+#'                            df.maxmind = download.maxmind())
+#' }
+addIPgeolocation <- function(ips = "", df.maxmind = data.frame(), boost = FALSE) {
+  # Para geolocalizar una IP en un rango comprobaremos si está entre la primera
+  # y la ultima ip de cada rango en MaxMind.
+
+  if (all(iptools::is_ipv4(ips))) {
+    ips <- iptools::ip_to_numeric(ips)
+  }
+  df <- data.frame(ip = as.numeric(ips))
+
+  if (boost) {
+    # Usamos multiples cpu's para geolocalizar IPs en rangos
+    no_cores <- parallel::detectCores() - 1
+    cl <- parallel::makeCluster(no_cores)
+    parallel::clusterExport(cl, "df.maxmind", envir = environment())
+    df$maxmind.rowname <- sapply(ips,
+                                 function(ip)
+                                   which((ip >= df.maxmind$min_numeric) &
+                                           (ip <= df.maxmind$max_numeric)))
+    parallel::stopCluster(cl)
+    rm(cl, no_cores)
+  } else {
+    df$maxmind.rowname <- sapply(ips,
+                                 function(ip)
+                                   which((ip >= df.maxmind$min_numeric) &
+                                           (ip <= df.maxmind$max_numeric)))
+  }
+
+  df <- dplyr::left_join(df, df.maxmind, by = c("maxmind.rowname" = "rowname"))
+
+  df <- dplyr::select(df, ip, network, latitude, longitude, accuracy_radius,
+                      is_anonymous_proxy, is_satellite_provider)
+
+  return(df)
+}
